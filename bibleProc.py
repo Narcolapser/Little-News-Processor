d2bee05f13dd9687ea3790994b2b7c374bccfc7a
Narcolapser <sandslash@gmail.com>
2014-02-08 00:00:16 +0000

    added PDF and HTML out support. HTML is in dire need of some help though. But with this I can officially declare that it is now at version 0.4! making progress.

--- bibleProc.py
+++ bibleProc.py
@@ -6,7 +6,7 @@ def consume(verses):
 		out = ""
 		for v in i:
 			add = str(v[3])
-			if v[0] == u'Proverbs':
+			if (v[0] == 'Proverbs'):
 				add = add.replace("\n\n","\n")
 				add += "\n"
 			out += add
---
+++ htmlOut.py
@@ -0,0 +1,7 @@
+def put(data,location):
+	f = open(location,"w")
+	f.write("<HTML>\n")
+	for i in data:
+		f.write("<p>"+i+"</p>\n")
+	f.write("</HTML>")
+	f.close()
